cf33336daf11553aab8d0183f577592ebdff01d6
rahadiankp <rahadian.k.putra@gmail.com>
2019-09-19 23:48:45 +0700

    add host uri to file automatically

--- tugas2/PyroFile_Server.py
+++ tugas2/PyroFile_Server.py
@@ -1,12 +1,16 @@
 from PyroFile import FileManager
 import Pyro4
 
+def write_uri_to_file(uri: str):
+    with open("pyro_host", "w") as fd:
+        fd.write(uri)
 
 def server_without_nameserver():
     daemon = Pyro4.Daemon()
     pyrofile_server = Pyro4.expose(FileManager)
     uri = daemon.register(pyrofile_server)
     print("PyroFile URI:", uri)
+    write_uri_to_file(uri.asString())
     daemon.requestLoop()
 
 
