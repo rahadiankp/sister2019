db9e0cd6fb7de9d3967c2ec338c82adb84aeba67
rahadiankp <rahadian.k.putra@gmail.com>
2019-09-22 12:57:11 +0700

    support ns for server

--- tugas2/PyroFile_Server.py
+++ tugas2/PyroFile_Server.py
@@ -8,13 +8,19 @@ def write_uri_to_file(uri: str):
         fd.write(uri)
 
 
-def server_without_nameserver(directory: str):
-    daemon = Pyro4.Daemon()
+def start_server(directory: str, with_ns=False, ns_host="localhost", ns_port=6969, name="pyrofileserver"):
     print("Using directory:", directory)
+    daemon = Pyro4.Daemon(host=ns_host)
     FileManager.DIRECTORY_PATH = directory
     uri = daemon.register(FileManager)
-    print("PyroFile URI:", uri)
+
+    if with_ns:
+        print("Using Pyro Nameserver...")
+        ns = Pyro4.locateNS(ns_host, ns_port)
+        ns.register(name, uri)
     write_uri_to_file(uri.asString())
+
+    print("OK PyroFile URI:", uri)
     daemon.requestLoop()
 
 
@@ -24,4 +30,4 @@ if __name__ == "__main__":
         print("DIRPATH\tRoot directory to expose")
         sys.exit(0)
     DIRECTORY_PATH = sys.argv[1]
-    server_without_nameserver(DIRECTORY_PATH)
+    start_server(DIRECTORY_PATH)
