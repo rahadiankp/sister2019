874ad68ff1a0ed3986edf3e9cb3f3a1a30aa9eec
rahadiankp <rahadian.k.putra@gmail.com>
2019-09-22 12:45:28 +0700

    dirpath issue

--- tugas2/PyroFile.py
+++ tugas2/PyroFile.py
@@ -1,4 +1,5 @@
-import os, glob
+import os
+import glob
 from random import randint
 import serpent
 from Pyro4 import expose
@@ -17,7 +18,7 @@ class FileManager(object):
     def get_listdir(self) -> list:
         file_list = []
         for filename in os.listdir(FileManager.DIRECTORY_PATH):
-            file_list.append((filename, os.path.getsize(FileManager.DIRECTORY_PATH + filename)))
+            file_list.append((filename, os.path.getsize(FileManager.DIRECTORY_PATH + "/" + filename)))
         return file_list
 
     def delete_file(self, filename: str) -> (bool, str):
@@ -25,10 +26,10 @@ class FileManager(object):
         try:
             # if globing enabled
             if "*" in filename:
-                for f in glob.iglob(FileManager.DIRECTORY_PATH+filename):
+                for f in glob.iglob(FileManager.DIRECTORY_PATH + "/" + filename):
                     os.remove(f)
             else:
-                os.remove(FileManager.DIRECTORY_PATH+filename)
+                os.remove(FileManager.DIRECTORY_PATH + "/" + filename)
             deleted = not deleted
             message = "File " + filename + " successfully deleted"
         except FileNotFoundError:
@@ -37,9 +38,9 @@ class FileManager(object):
         return deleted, message
 
     def create_file(self, filename: str) -> (bool, str):
-        if os.path.isfile(FileManager.DIRECTORY_PATH+filename):
+        if os.path.isfile(FileManager.DIRECTORY_PATH + "/" + filename):
             return False, "File " + filename + " already exists"
-        new_file = open(FileManager.DIRECTORY_PATH+filename, "w")
+        new_file = open(FileManager.DIRECTORY_PATH + "/" + filename, "w")
         new_file.close()
         return True, "File " + filename + " successfully created"
 
@@ -47,13 +48,13 @@ class FileManager(object):
         try:
             size = 0
             content = b""
-            with open(FileManager.DIRECTORY_PATH+filename, "rb") as fd:
+            with open(FileManager.DIRECTORY_PATH + "/" + filename, "rb") as fd:
                 buffer = fd.read(1024)
                 while buffer != b"":
                     size += len(buffer)
                     content = content + buffer
                     buffer = fd.read(1024)
-            return True, FileManager.DIRECTORY_PATH+filename + " OK", size, content
+            return True, FileManager.DIRECTORY_PATH + "/" + filename + " OK", size, content
         except FileNotFoundError:
             return False, "File " + filename + " could not be found", -1, b""
 
@@ -63,15 +64,15 @@ class FileManager(object):
         try:
             # pseudo-COW
             temp_filename = ".tempfile-"+str(randint(420, 42069))
-            with open(FileManager.DIRECTORY_PATH+temp_filename, "wb") as temp_fd:
+            with open(FileManager.DIRECTORY_PATH + "/" + temp_filename, "wb") as temp_fd:
                 write_size = temp_fd.write(content)
             # check if write size doesn't match
             if write_size != size:
                 return False, "Error on writing", -1
             # delete original file
-            self.delete_file(filename)
+            self.delete_file(FileManager.DIRECTORY_PATH + "/" + filename)
             # rename tempfile to filename
-            os.rename(FileManager.DIRECTORY_PATH+temp_filename, FileManager.DIRECTORY_PATH+filename)
+            os.rename(FileManager.DIRECTORY_PATH + "/" + temp_filename, FileManager.DIRECTORY_PATH + "/" + filename)
             return True, "File " + filename + " updated", write_size
         except FileNotFoundError:
             return False, "File " + filename + " could not be found", -1
