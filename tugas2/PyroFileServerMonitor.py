25907a9c47929b431ce959d0a1a09605eedcfbd6
rahadiankp <rahadian.k.putra@gmail.com>
2019-10-15 18:27:34 +0700

    delete garbage

--- tugas2/PyroFileServerMonitor.py
+++
@@ -1,6 +0,0 @@
-class Monitor(object):
-    def __init__(self, type: str = "pingack", host: str = "localhost", port: int = 60001):
-        self.type = type
-        self.host = host
-        self.port = port
-        self.ping
